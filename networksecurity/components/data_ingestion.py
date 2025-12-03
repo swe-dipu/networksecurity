e50b27564c134a1896b7de07b1447e6a4f419d85
Dipu Ghosh <swe.dipu@gmail.com>
2025-12-04 03:19:08 +0600

    Data Validation conpleted

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -2,11 +2,10 @@ from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 
 
-## Configuration of the Data Ingestion Config
+## configuration of the Data Ingestion Config
 
 from networksecurity.entity.config_entity import DataIngestionConfig
 from networksecurity.entity.artifact_entity import DataIngestionArtifact
-
 import os
 import sys
 import numpy as np
@@ -14,8 +13,6 @@ import pandas as pd
 import pymongo
 from typing import List
 from sklearn.model_selection import train_test_split
-
-
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -28,11 +25,10 @@ class DataIngestion:
             self.data_ingestion_config=data_ingestion_config
         except Exception as e:
             raise NetworkSecurityException(e,sys)
-    
-
+        
     def export_collection_as_dataframe(self):
         """
-        Read data from MongoDB
+        Read data from mongodb
         """
         try:
             database_name=self.data_ingestion_config.database_name
@@ -42,56 +38,56 @@ class DataIngestion:
 
             df=pd.DataFrame(list(collection.find()))
             if "_id" in df.columns.to_list():
-                df=df.drop(columns="_id",axis=1)
-
+                df=df.drop(columns=["_id"],axis=1)
+            
             df.replace({"na":np.nan},inplace=True)
             return df
         except Exception as e:
             raise NetworkSecurityException
         
-
     def export_data_into_feature_store(self,dataframe: pd.DataFrame):
         try:
             feature_store_file_path=self.data_ingestion_config.feature_store_file_path
             #creating folder
-            dir_path=os.path.dirname(feature_store_file_path)
+            dir_path = os.path.dirname(feature_store_file_path)
             os.makedirs(dir_path,exist_ok=True)
             dataframe.to_csv(feature_store_file_path,index=False,header=True)
             return dataframe
-        
+            
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-
     def split_data_as_train_test(self,dataframe: pd.DataFrame):
         try:
             train_set, test_set = train_test_split(
-                dataframe,test_size=self.data_ingestion_config.train_test_split_ratio
+                dataframe, test_size=self.data_ingestion_config.train_test_split_ratio
             )
             logging.info("Performed train test split on the dataframe")
 
-            logging.info("Exited split_data_as_traintest method of Data_Ingestion Class")
-
-            dir_path=os.path.dirname(self.data_ingestion_config.training_file_path)
-
-            os.makedirs(dir_path,exist_ok=True)
-
-            logging.info(f"Exporting train and test file path")
-
+            logging.info(
+                "Exited split_data_as_train_test method of Data_Ingestion class"
+            )
+            
+            dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
+            
+            os.makedirs(dir_path, exist_ok=True)
+            
+            logging.info(f"Exporting train and test file path.")
+            
             train_set.to_csv(
                 self.data_ingestion_config.training_file_path, index=False, header=True
             )
 
-            test_set.to_csv(self.data_ingestion_config.testing_file_path, index=True, header=True)
-
+            test_set.to_csv(
+                self.data_ingestion_config.testing_file_path, index=False, header=True
+            )
             logging.info(f"Exported train and test file path.")
 
-
+            
         except Exception as e:
-            raise NetworkSecurityException
-
-
-
+            raise NetworkSecurityException(e,sys)
+        
+        
     def initiate_data_ingestion(self):
         try:
             dataframe=self.export_collection_as_dataframe()
